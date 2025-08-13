006284e3a709bbc41c4b2d526a333e46e9bb388e
nerio.baez <nbaez001@gamimail.com>
2025-08-13 00:59:24 -0500

    Cambios

---
+++ reactive-hello/src/main/java/com/empresa/proyecto/buffering/Switching.java
@@ -0,0 +1,22 @@
+package com.empresa.proyecto.buffering;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class Switching {
+    public static void main(String[] args) throws InterruptedException {
+        Observable<String> source = Observable.just("John", "Lily", "Emma", "Reyan", "Darshin")
+                .concatMap(s -> Observable.just(s)
+                        .delay(ThreadLocalRandom.current().nextInt(1000), TimeUnit.MILLISECONDS));
+
+        Observable.interval(2, TimeUnit.SECONDS)
+                .switchMap(s -> source.doOnDispose(
+                        () -> System.out.println("Disposing and starting again!")
+                ))
+                .subscribe(System.out::println);
+
+        Thread.sleep(10000);
+    }
+}
